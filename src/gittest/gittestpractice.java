9eb1a30909a470ec43d834cbe2cd7eb06f2ecfcd
kaki <jeyvonblut93@gmail.com>
2022-12-13 11:49:33 +0200

    test12

---
+++ src/gittest/gittestpractice.java
@@ -0,0 +1,13 @@
+package gittest;
+
+public class gittestpractice {
+
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <100; i++){
+            System.out.println(i);
+        }
+//4
+    }
+}
